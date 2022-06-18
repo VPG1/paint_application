cdad738f218bd866bb2eae8502a431f8f066976a
Kirill <aaaa@gmail.ru>
2022-06-18 14:07:28 +0300

    add pen width field in user settings struct

--- usersettings.h
+++ usersettings.h
@@ -1,11 +1,12 @@
 #ifndef USERSETTINGS_H
 #define USERSETTINGS_H
 
+#include <QColor>
 
-class UserSettings
-{
-public:
-    UserSettings();
+// структура
+struct UserSettings{
+    QColor color = Qt::black;
+    int penWidth = 10;
 };
 
 #endif // USERSETTINGS_H
