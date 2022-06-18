ba12709e69000aae122026c4e53139344c3b6673
Kirill <aaaa@gmail.ru>
2022-06-18 15:02:57 +0300

    scribblearea implementation

--- scribblearea.cpp
+++ scribblearea.cpp
@@ -1,7 +1,54 @@
 #include "scribblearea.h"
 
-ScribbleArea::ScribbleArea(QWidget *parent)
-    : QWidget{parent}
+ScribbleArea::ScribbleArea(UserSettings &userSetting, QWidget *parent)
+    :  QWidget(parent), image(200, 200, QImage::Format_RGB32), userSettings(userSetting)
 {
+    image.fill(Qt::white);
+    // содержимое находится в верхнем левом углу и не изменеяется при изменении размеров окна
+    setAttribute(Qt::WA_StaticContents);
+}
 
+void ScribbleArea::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    QRect boudingRect = event->rect(); // область которую нужно перерисовать
+    painter.drawImage(boudingRect, image, boudingRect);
+}
+
+void ScribbleArea::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton){
+        lastPoint = event->pos();
+        sribbling = true;
+    }
+}
+
+void ScribbleArea::mouseMoveEvent(QMouseEvent *event)
+{
+    if((event->buttons() & Qt::LeftButton) && sribbling){
+        drawLineTo(event->pos());
+    }
+}
+
+void ScribbleArea::mouseReleaseEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton && sribbling){
+        drawLineTo(event->pos());
+        sribbling = false;
+    }
+}
+
+void ScribbleArea::drawLineTo(QPoint endPoint)
+{
+    QPainter painter(&image);
+    painter.setPen(QPen(userSettings.color, userSettings.penWidth,
+                        Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
+    painter.drawLine(lastPoint, endPoint);
+
+    // обновляем область в которой нарисовали линию
+    // учитываем penWidth при обновление
+    int rad = (userSettings.penWidth / 2) + 2;
+    update(QRect(lastPoint, endPoint).normalized().adjusted(-rad, -rad, rad, rad));
+
+    lastPoint = endPoint;
 }
--- scribblearea.h
+++ scribblearea.h
@@ -2,15 +2,32 @@
 #define SCRIBBLEAREA_H
 
 #include <QWidget>
+#include <QPaintEvent>
+#include <QMouseEvent>
+#include <QPainter>
+#include "usersettings.h"
 
 class ScribbleArea : public QWidget
 {
     Q_OBJECT
 public:
-    explicit ScribbleArea(QWidget *parent = nullptr);
+    ScribbleArea(UserSettings &userSetting, QWidget *parent = nullptr);
 
-signals:
+protected:
+    void paintEvent(QPaintEvent *) override;
+    void mousePressEvent(QMouseEvent *) override;
+    void mouseMoveEvent(QMouseEvent *) override;
+    void mouseReleaseEvent(QMouseEvent *) override;
 
+private:
+    QImage image; // переменная для хранения рисунка пользователя
+
+    QPoint lastPoint;
+    bool sribbling = false; // происходит ли рисование в данный момент времени
+
+    UserSettings &userSettings;
+
+    void drawLineTo(QPoint endPoint);
 };
 
 #endif // SCRIBBLEAREA_H
