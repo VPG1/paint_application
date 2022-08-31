90fb29808848e92780f48d60b9afe04725a5ffec
Antilevskaya-Ksenia-153501 <antikevkun@gmail.com>
2022-08-31 16:47:29 +0300

    design of app

--- Widgets/mainwindow.cpp
+++ Widgets/mainwindow.cpp
@@ -12,7 +12,8 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(m_scribbleArea);
 
     // init draw actions
-    connect(ui->toolBar->addAction("pen"),
+
+    /*connect(ui->toolBar->addAction("pen"),
             &QAction::triggered, this,  &MainWindow::chosePen);
     connect(ui->toolBar->addAction("line"),
             &QAction::triggered, this, &MainWindow::choseLine);
@@ -25,9 +26,69 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->toolBar->addAction("open"),
             &QAction::triggered, this, &MainWindow::open);
     connect(ui->toolBar->addAction("save"),
-            &QAction::triggered, this, &MainWindow::save);
+            &QAction::triggered, this, &MainWindow::save);*/
 
 
+
+
+    QAction* pen = new QAction(this);
+    pen->setIcon(QIcon(":/Icons/pencil.png"));
+    pen->setIconText("pencil");
+    ui->toolBar->addAction(pen);
+    connect(pen, &QAction::triggered, this,  &MainWindow::chosePen);
+    QCursor cursorPencil = QCursor(QPixmap(":/Icons/pencil_cursor.png"));
+    this->setCursor(cursorPencil);
+
+    //QCursor cursorPencil = QCursor(QPixmap(":/Icons/pencil.png"));
+    //ui->toolBar->setCursor(cursorPencil);
+    //this->setCursor(cursorPencil);
+
+    QAction* line = new QAction(this);
+    line->setIcon(QIcon(":/Icons/line.png"));
+    line->setIconText("line");
+    ui->toolBar->addAction(line);
+    connect(line, &QAction::triggered, this, &MainWindow::choseLine);
+
+    QAction* rectangle = new QAction(this);
+    rectangle->setIcon(QIcon(":/Icons/rectangle.png"));
+    rectangle->setIconText("rectangle");
+    ui->toolBar->addAction(rectangle);
+    connect(rectangle, &QAction::triggered, this, &MainWindow::choseRectangle);
+
+    QAction* ellipse = new QAction(this);
+    ellipse->setIcon(QIcon(":/Icons/ellipse.png"));
+    ellipse->setIconText("ellipse");
+    ui->toolBar->addAction(ellipse);
+    connect(ellipse, &QAction::triggered, this, &MainWindow::choseEllipse);
+
+    QAction* floodfill = new QAction(this);
+    floodfill->setIcon(QIcon(":/Icons/pouring paint.png"));
+    floodfill->setIconText("floodfill");
+    ui->toolBar->addAction(floodfill);
+    connect(floodfill, &QAction::triggered, this, &MainWindow::choseFloodFill);
+
+    QAction* clear = new QAction(this);
+    clear->setIcon(QIcon(":/Icons/clear.png"));
+    clear->setIconText("clear all");
+    ui->toolBar->addAction(clear);
+    connect(clear, &QAction::triggered, m_scribbleArea, &ScribbleArea::clear);
+
+    QAction* load = new QAction(tr("&Load"), this);
+    QAction* save = new QAction(tr("&Save"), this);
+    QAction* info_paint = new QAction(tr("&About Qt Paint"), this);
+    QMenu* file = new QMenu(tr("&File"), menuBar());
+    QMenu* info = new QMenu(tr("&About"), menuBar());
+    menuBar()->addMenu(file);
+    menuBar()->addMenu(info);
+    file->addAction(load);
+    file->addAction(save);
+    info->addAction(info_paint);
+    connect(load, &QAction::triggered, this, &MainWindow::open);
+    connect(save, &QAction::triggered, this, &MainWindow::save);
+    connect(info_paint, &QAction::triggered, this, &MainWindow::about);
+    load->setShortcut(QKeySequence::Open);
+    save->setShortcut(QKeySequence::Save);
+    save->setIcon(QPixmap(":/Icons/save.png"));
 
 
     // init change color action
@@ -46,12 +107,18 @@ MainWindow::MainWindow(QWidget *parent)
     QAction *undoAction = new QAction("undo");
     undoAction->setShortcut(QKeySequence::Undo);
     ui->toolBar->addAction(undoAction);
+    undoAction->setIcon(QIcon(":/Icons/undo.png"));
+    undoAction->setIconText("undo");
     connect(undoAction, &QAction::triggered, m_scribbleArea, &ScribbleArea::undo);
 
     QAction *redoAction = new QAction("redo");
     redoAction->setShortcut(QKeySequence::Redo);
     ui->toolBar->addAction(redoAction);
+    redoAction->setIcon(QIcon(":/Icons/redo.png"));
+    redoAction->setIconText("redo");
     connect(redoAction, &QAction::triggered, m_scribbleArea, &ScribbleArea::redo);
+
+    ui->toolBar->unsetCursor();
 }
 
 MainWindow::~MainWindow()
@@ -70,26 +137,33 @@ void MainWindow::changingColor()
 void MainWindow::chosePen()
 {
     UserSettings::getInstance()->drawStrategy = std::make_unique<PenDrawStrategy>();
+    QCursor cursorPencil = QCursor(QPixmap(":/Icons/pencil_cursor.png"));
+    this->setCursor(cursorPencil);
 }
 
 void MainWindow::choseLine()
 {
     UserSettings::getInstance()->drawStrategy = std::make_unique<LineDrawStrategy>();
+    this->unsetCursor();
 }
 
 void MainWindow::choseRectangle()
 {
     UserSettings::getInstance()->drawStrategy = std::make_unique<RectangleDrawStrategy>();
+    this->unsetCursor();
 }
 
 void MainWindow::choseEllipse()
 {
     UserSettings::getInstance()->drawStrategy = std::make_unique<EllipseDrawStrategy>();
+    this->unsetCursor();
 }
 
 void MainWindow::choseFloodFill()
 {
     UserSettings::getInstance()->drawStrategy = std::make_unique<FloodFillStrategy>();
+    QCursor cursorFloodFill = QCursor(QPixmap(":/Icons/pouring paint_cursor.png"));
+    this->setCursor(cursorFloodFill);
 }
 void MainWindow::open()
 {
@@ -97,7 +171,7 @@ void MainWindow::open()
         QString fileName = QFileDialog::getOpenFileName(this,
                            tr("Open File"), QDir::currentPath());
         if (!fileName.isEmpty())
-            scribbleArea->openImage(fileName);
+            m_scribbleArea->openImage(fileName);
     }
 }
 void MainWindow::save()
@@ -108,7 +182,7 @@ void MainWindow::save()
 }
 bool MainWindow::maybeSave()
 {
-    if (scribbleArea->isModified()) {
+    if (m_scribbleArea->isModified()) {
        QMessageBox::StandardButton res;
        res = QMessageBox::warning(this, tr("PaintApplication"),
                           tr("The image has been modified.\n"
@@ -122,6 +196,16 @@ bool MainWindow::maybeSave()
     }
     return true;
 }
+
+void MainWindow::about()
+{
+    QMessageBox::StandardButton res = QMessageBox::information(this, tr("PaintApplication"),
+                       tr("Application is written in C++, using QT.\n"
+                          "Originally made for project in university.\n\n"
+                          "Version 1.0.0"),
+                       QMessageBox::Ok);
+}
+
 bool MainWindow::saveFile(const QByteArray &fileFormat)
 {
     QString initialPath = QDir::currentPath() + "/untitled." + fileFormat;
@@ -133,7 +217,7 @@ bool MainWindow::saveFile(const QByteArray &fileFormat)
                                .arg(QString::fromLatin1(fileFormat)));
     if (fileName.isEmpty())
         return false;
-    return scribbleArea->saveImage(fileName, fileFormat.constData());
+    return m_scribbleArea->saveImage(fileName, fileFormat.constData());
 }
 
 
